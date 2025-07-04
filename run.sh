c8cbeb16bb4f5abd01cbe99a82397e047bc4ad8f
scattlights <169085685+scattlights@users.noreply.github.com>
2025-07-04 15:38:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 while true; do
   echo
+  echo "1. 修改SSH端口"
+  echo "2. 系统更新和清理"
+  echo "3. 安装UFW并启用"
+  echo "4. UFW防火墙端口管理"
+  echo "5. 清理日志和缓存"
+  echo "6. 查看或修改当前时区"
+  echo "7. 安装Nginx"
+  echo "8. 卸载Nginx"
+  echo
   read -rp "请输入要执行的脚本编号（0退出）： " num
 
   if [[ ! "$num" =~ ^[0-9]+$ ]]; then
