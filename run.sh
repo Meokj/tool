de95a5a634eef23e3317bbaedb2090afcef37d6d
Meodj <169085685+Meodj@users.noreply.github.com>
2025-07-19 04:56:49 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,12 +3,13 @@ while true; do
   echo "=================="
   echo "1. 修改SSH端口"
   echo "2. 系统更新和清理"
-  echo "3. 安装UFW并启用"
+  echo "3. 安装UFW防火墙并启用"
   echo "4. UFW防火墙端口管理"
   echo "5. 清理日志和缓存"
   echo "6. 查看或修改当前时区"
   echo "7. 安装Nginx"
   echo "8. 卸载Nginx"
+  echo "9. 卸载UFW防火墙"
   echo "=================="
   read -rp "请输入要执行的脚本编号（0退出）： " num
 
